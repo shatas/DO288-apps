f6391a14103d70aeb26c78e59a8db281653e5160
Student User <student@workstation.lab.example.com>
2020-05-14 08:31:14 +0000

    test

--- java-serverhost/src/main/java/com/redhat/training/example/javaserverhost/rest/RestApplication.java
+++ java-serverhost/src/main/java/com/redhat/training/example/javaserverhost/rest/RestApplication.java
@@ -1,9 +1,1 @@
-package com.redhat.training.example.javaserverhost.rest;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/")
-public class RestApplication extends Application {
-
-}
+package com.redhat.training.example.javaserverhost.rest; import javax.ws.rs.Path; import javax.ws.rs.core.Response; import javax.ws.rs.GET; import javax.ws.rs.Produces; import java.net.InetAddress; @Path("/") public class ServerHostEndPoint {  @GET  @Produces("text/plain")  public ResponsedoGet() {  String host = "";  try {  host = InetAddress.getLocalHost().getHostName()  }  catch (Eception e) {  e.printStackTrace();  }  String msg = "I am running on server "+host+" Version 1.0 \n";  return Response.ok(msg).build();  } }
